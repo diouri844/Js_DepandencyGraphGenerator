2750136f177dd23d65d731067abd6ecd12d4f167
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-11 21:02:57 +0100

    Refactor scanDirectory function to return structured ScanneResult and add generateDot function for graph schema generation

--- src/scanner/directory.ts
+++ src/scanner/directory.ts
@@ -1,27 +1,73 @@
 
 import { readdir } from 'fs/promises';
 import * as fs from 'fs';
-import { join } from 'path';
+import path, { join } from 'path';
 import { ScanneResult } from '../types/scanne';
+import { DependencyGraph } from '../types/graph';
+//import { DependencyGraph } from '../types/graph';
 
-async function scanDirectory(directory: string):Promise<ScanneResult[]> {
-    const files = await readdir(directory, { withFileTypes: false });
-    const result: ScanneResult[] = [];
+async function scanDirectory(directory: string): Promise<ScanneResult> {
+    const files = await readdir(directory, { withFileTypes: true });
+    const result: ScanneResult = {
+        path: directory,
+        directory: [],
+        files: []
+    };
     for (const file of files) {
-        if (file.startsWith('.') || file === 'node_modules') {
+        if (file.name.startsWith('.') || file.name.endsWith(".pyc")|| file.name === 'node_modules' || file.name === "dist") {
             continue;
         }
-        const filePath = join(directory, file);
+        const filePath = join(directory, file.name);
         const stat = await fs.promises.stat(filePath);
+
         if (stat.isDirectory()) {
-            const subfiles = await scanDirectory(filePath);
-            //console.log(filePath, subfiles);
-            result.push({ type: 'directory', path: filePath, files: subfiles });
+            // add this dir to the result : 
+            // create subFolder : 
+            const subFolder: ScanneResult = await scanDirectory(filePath);
+            // call scanDir again for this file dir : 
+            result.directory.push(subFolder);
         } else {
-            result.push({ type: 'file', path: filePath, files:null });
+            result.files.push(file.name);
         }
     }
     return result;
 }
 
-export { scanDirectory };
+
+
+
+
+
+
+// function for genarating the graph schema based on scanned directory  : 
+
+
+
+
+function generateDot(graph: DependencyGraph): string {
+    let dot = 'digraph G {\n';
+
+    for (const [module, dependencies] of Object.entries(graph)) {
+        if (dependencies.length === 0) {
+            dot += `  "${module}";\n`;
+        } else {
+            dependencies.forEach(dep => {
+                dot += `  "${module}" -> "${dep}";\n`;
+            });
+        }
+    }
+
+    dot += '}';
+    return dot;
+}
+
+
+
+
+
+
+
+
+
+
+export { scanDirectory, generateDot};
