d897cea3bb089ff6b17b49ac36374658aa295e3b
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-11 23:54:21 +0100

    Refactor generateGraph function to improve parent-child relationship handling in dependency graph traversal

--- src/graph/generateGraph.ts
+++ src/graph/generateGraph.ts
@@ -7,16 +7,26 @@ export function generateGraph(scanResult: ScanneResult): DependencyGraph {
 
     function traverse(result: ScanneResult, parent: string | null) {
         let currentPath = result.path;
-        if(currentPath.includes(path.sep)){
-            currentPath = currentPath.split(path.sep)[1];
+        if (currentPath.includes(path.sep)) {
+            parent = currentPath.split(path.sep)[0];
+            currentPath = currentPath.split(path.sep).slice(1).join(path.sep);
         }
-        graph[currentPath] = result.files;
+        if (!parent) {
+            graph[currentPath] = result.files;
+        } else {
+            if (!graph[parent]) {
+                graph[parent] = [];
+            }
+            graph[parent].push(currentPath);
+            graph[currentPath] = result.files;
+        }
 
         for (const subDir of result.directory) {
-            traverse(subDir,null);
+            traverse(subDir, currentPath);
         }
     }
-
-    traverse(scanResult, null);
+    const arr = scanResult.path.split(path.sep);
+    const parent = arr[arr.length-1];
+    traverse(scanResult, parent);
     return graph;
 }
